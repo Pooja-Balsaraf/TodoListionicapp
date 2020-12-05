445cdf4899ce89b47b5474700a376a00a9b918ea
Pooja Balsaraf <poojabalsaraf10@gmail.com>
2020-12-05 20:44:16 +0530

    crist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,30 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomePage } from './home/home.page';
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    path:'',
+    redirectTo: 'crit',
+    pathMatch: 'full'
+  },
+  {
+    path: 'crit',
+    loadChildren: () => import('./crit/crit.module').then( m => m.CritPageModule)
+  },
+  {
+    path: 'later',
+    loadChildren: () => import('./later/later.module').then( m => m.LaterPageModule)
+  },
+  {
+    path: 'done',
+    loadChildren: () => import('./done/done.module').then( m => m.DonePageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
   }
+  
 ];
 @NgModule({
   imports: [
